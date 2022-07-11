faf5be650cbc3ad11026822f3b94f57c4b2a78f0
kamataryo <kamataryo@users.noreply.github.com>
2022-07-11 11:42:11 +0900

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Prop ID CLI
+
+## Setup
+
+```shell
+# authentication information
+PROP_ID_API_KEY=aaaa
+PROP_ID_ACCESS_TOKEN=bbbb
+```
+
+## Usage
+
+```shell
+# with CLI arguments
+$ npx prop-id-cli '13-0123-4567-89ab-cdef'
+# with STDIN
+$ echo '13-0123-4567-89ab-cdef' | npx prop-id-cli
+```
---
+++ ref.mjs
@@ -0,0 +1,41 @@
+import fs from 'fs/promises'
+import fetch from 'node-fetch'
+const { PROP_ID_API_KEY, PROP_ID_ACCESS_TOKEN } = process.env
+
+const STDIN_TIMEOUT = 50
+
+const main = async () => {
+
+	let propId = process.argv[2]
+
+	if(!propId) {
+		process.stdin.resume();
+		process.stdin.setEncoding('utf8');
+	
+		let data = ''
+		process.stdin.on('data', chunk => data += chunk)
+		await Promise.race([
+			new Promise(resolve => process.stdin.on('end', resolve)),
+			new Promise(resolve => setTimeout(resolve, STDIN_TIMEOUT)),
+		])
+		propId = data.replace(/[\n\r]+$/, '')
+	
+	}
+
+	if(!propId) {
+		throw new Error('No propId.')
+	}
+
+	const resp = await fetch(`https://api.propid.jp/v1/${propId}?api-key=${PROP_ID_API_KEY}`, {
+		"headers": {
+			'x-access-token': PROP_ID_ACCESS_TOKEN
+		}
+	});
+	if(resp.status > 399) {
+		throw new Error(`Request with ${propId} ends with status code ${resp.status}.`)
+	}
+	const body = await resp.json()
+	process.stdout.write(JSON.stringify(body) + '\n')
+}
+
+main()
